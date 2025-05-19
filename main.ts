20c1f534956290d738749cd4e9a15b18f8daf68f
Fernando <fernando.inversionmf@gmail.com>
2025-05-18 20:01:41 -0700

    Defining the tools

--- main.ts
+++ main.ts
@@ -11,3 +11,42 @@ const server = new McpServer({
     version: "1.0.0"
 });
 
+// 2. Define the tools
+//Allow the LLM to perform actions through your server
+server.tool(
+    "fetch-weather", //Tool title
+    "Tool to fetch the weather of a city", //Tool description
+    // Tool parameters
+    {
+        city: z.string().describe("City name") 
+    },
+    // Tool function
+    async ( {city } ) => {
+        const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`)
+        const data = await response.json();
+        
+        if(data.length === 0){
+            return {
+                content: [
+                    {
+                        type: "text",
+                        text: `No information found for the city of ${city}`
+                    }
+                ]
+            }
+        }
+
+        const { latitude, longitude } = data.results[0];
+        const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current=temperature_2m,precipitation,is_day,rain&past_days=5`)
+        const weatherData = await weatherResponse.json();
+        
+        return {
+            content: [
+                {
+                    type: "text",
+                    text: JSON.stringify(weatherData, null, 2)
+                }
+            ]
+        }
+    }
+)
